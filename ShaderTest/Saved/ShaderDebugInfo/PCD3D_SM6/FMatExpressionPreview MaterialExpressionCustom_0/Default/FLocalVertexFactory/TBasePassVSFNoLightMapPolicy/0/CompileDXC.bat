4e0aa6052bc9953d619f6050bdab051f5d595216
406747661 <406747661@qq.com>
2024-01-26 17:02:30 +0800

    ue5.2
    ue5.2

--- ShaderTest/Saved/ShaderDebugInfo/PCD3D_SM6/FMatExpressionPreview MaterialExpressionCustom_0/Default/FLocalVertexFactory/TBasePassVSFNoLightMapPolicy/0/CompileDXC.bat
+++ ShaderTest/Saved/ShaderDebugInfo/PCD3D_SM6/FMatExpressionPreview MaterialExpressionCustom_0/Default/FLocalVertexFactory/TBasePassVSFNoLightMapPolicy/0/CompileDXC.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
-SET DXC="E:\Program Files\Epic Games\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
+SET DXC="C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64\dxc.exe"
 IF NOT EXIST %DXC% (
-	ECHO Couldn't find dxc.exe under "E:\Program Files\Epic Games\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
+	ECHO Couldn't find dxc.exe under "C:\Program Files\Epic Games\UE_5.1\Engine\Binaries\ThirdParty\ShaderConductor\Win64"
 	GOTO :END
 )
 %DXC% /auto-binding-space 0 /Zpr /O3 -Wno-parentheses-equality /T vs_6_6 /E Main /Fc BasePassVertexShader.d3dasm /Fo BasePassVertexShader.dxil BasePassVertexShader.usf
